c9fe5b51a5d19729e37becaec138c515e9c2319b
itsjkl <secretivewalling@gmail.com>
2024-07-26 15:25:20 +0300

    Update users.js

--- api/users.js
+++ api/users.js
@@ -1,1 +1,24 @@
+javascriptCopylet users = {};
 
+function cleanupExpiredUsers() {
+  const now = Date.now();
+  for (const [username, lastSeen] of Object.entries(users)) {
+    if (now - lastSeen > 10000) {
+      delete users[username];
+    }
+  }
+}
+
+export default async function handler(req, res) {
+  cleanupExpiredUsers();
+
+  if (req.method === 'POST') {
+    const { username } = req.body;
+    users[username] = Date.now();
+    res.status(200).json({ message: 'User updated' });
+  } else if (req.method === 'GET') {
+    res.status(200).json(Object.keys(users));
+  } else {
+    res.status(405).json({ message: 'Method not allowed' });
+  }
+}
